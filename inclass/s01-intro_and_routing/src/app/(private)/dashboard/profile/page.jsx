35470def9b9d77947d1960960d24999242142974
ayseugurlu <ayseugurlu9135@gmail.com>
2024-12-31 23:52:12 +0100

    update

--- inclass/s01-intro_and_routing/src/app/(private)/dashboard/profile/page.jsx
+++ inclass/s01-intro_and_routing/src/app/(private)/dashboard/profile/page.jsx
@@ -1,16 +1,20 @@
 import GoBack from "@/components/GoBack";
 import React from "react";
 
-const Users = () => {
+export const metadata = {
+  title: "Profile Page",
+  description: "This is Profile page",
+};
+const Profile = () => {
   return (
     <div className="container mx-auto px-6 py-6">
       <h1 className="text-3xl font-bold underline text-center">
-        Hello, this is Users Page
+        Hello, this is Profile Page
       </h1>
-      
+
       <GoBack />
     </div>
   );
 };
 
-export default Users;
+export default Profile;
