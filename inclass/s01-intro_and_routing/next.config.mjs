1f9e2b1b9c2e830241f780025d1f254275cb90a6
ayseugurlu <ayseugurlu9135@gmail.com>
2024-12-31 23:53:35 +0100

    up

--- inclass/s01-intro_and_routing/next.config.mjs
+++ inclass/s01-intro_and_routing/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "clarusway.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
