aa8e4fff4ea9ef7120cfe8fb65dd3c35cea170d7
ayseugurlu <ayseugurlu9135@gmail.com>
2025-01-02 13:42:33 +0100

    authentication add

---
+++ inclass/s03-netflix/src/app/layout.js
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Navbar from "@/components/Navbar";
+import AuthContextProvider from "@/context/AuthContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Netflix",
+  description: "A movie platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+          <ToastContainer />
+        </AuthContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ inclass/s03-netflix/src/context/AuthContext.js
@@ -0,0 +1,114 @@
+"use client"
+import { createContext, useContext,useEffect, useState } from "react"
+import {
+  createUserWithEmailAndPassword,signInWithEmailAndPassword,GoogleAuthProvider,signInWithPopup,onAuthStateChanged
+ 
+} from "firebase/auth";
+import { auth } from "@/auth/firebase";
+import { toastSuccessNotify,toastErrorNotify } from "@/helpers/ToastNotify";
+import { useRouter } from "next/navigation";
+// auth(yetki) işlemlerini yaptığımız context
+export const YetkiContext=createContext()
+
+//* custom hook
+export const useAuthContext=()=>{
+
+    return useContext(YetkiContext)
+}
+
+
+const AuthContextProvider=({children})=>{
+const [currentUser, setCurrentUser]=useState("")
+
+const router=useRouter()
+
+
+useEffect(()=>{
+    userTakip()
+},[])
+
+
+
+    const createKullanici=async(email,password,displayName)=>{
+
+
+      try {
+
+      await  createUserWithEmailAndPassword(auth,email,password)
+
+  await updateProfile(auth.currentUser, {
+        displayName,
+      });
+
+
+toastSuccessNotify("register başarılı")
+router.push("/login")
+
+
+
+        
+      } catch (error) {
+        
+       toastErrorNotify(error.message) 
+      }
+
+    }
+
+    const signIn=async(email,password)=>{
+
+ try {
+
+  await   signInWithEmailAndPassword(auth, email, password)
+
+toastSuccessNotify("login başarılı")
+
+router.push("/profile")
+
+        
+      } catch (error) {
+        
+       toastErrorNotify(error.message) 
+      }
+
+
+    }
+
+const signInGoogle=()=>{
+
+const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+   router.push("/profile")
+   toastSuccessNotify("google girişi başarılı")
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+
+
+
+}
+
+
+const userTakip=()=>{
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+  const  {email,displayName,photoURL}=user
+
+setCurrentUser({email,displayName,photoURL})
+
+
+  } else {
+    setCurrentUser(false)
+
+  }
+});
+
+}
+
+
+
+  return <YetkiContext.Provider value={{createKullanici,signIn,signInGoogle}}> {children} </YetkiContext.Provider>  
+}
+export default AuthContextProvider;
